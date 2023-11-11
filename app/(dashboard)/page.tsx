36dc9e459c0dbb5c708d541a17b2136421120946
rbpolim <rbpolim@gmail.com>
2023-11-11 11:27:28 -0300

    feat: installed shadcn

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 const Home = () => {
   return (
     <div>
       <h1>Senac</h1>
+      <Button>
+        CLICK HERE
+      </Button>
     </div>
   )
 }
