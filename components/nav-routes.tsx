08b5a158412c49decace69d044315f2041463e72
rbpolim <rbpolim@gmail.com>
2023-11-11 12:43:06 -0300

    feat: nav-router component

---
+++ components/nav-routes.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import {
+  BadgeDollarSign,
+  FolderClosed,
+  Home,
+  PlusCircle,
+  Settings,
+  UserSquare2
+} from "lucide-react"
+
+import { NavRoutesItem } from "@/components/nav-routes-item"
+
+export const NavRoutes = () => {
+  const routes = [
+    {
+      icon: Home,
+      label: "Home",
+      href: "/",
+    },
+    {
+      icon: Settings,
+      label: "Nota e Frequência",
+      href: "/grades",
+    },
+    {
+      icon: UserSquare2,
+      label: "Aulas",
+      href: "/classes",
+    },
+    {
+      icon: PlusCircle,
+      label: "Nova Matricula",
+      href: "/registration",
+    },
+    {
+      icon: BadgeDollarSign,
+      label: "Financeiro",
+      href: "/financial",
+    },
+    {
+      icon: FolderClosed,
+      label: "Meus Documentos",
+      href: "/documents",
+    },
+  ]
+
+  return (
+    <div className="flex flex-col w-full mt-10">
+      {routes.map((route) => (
+        <NavRoutesItem
+          key={route.href}
+          {...route}
+        />
+      ))}
+    </div>
+  )
+}
