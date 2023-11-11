2b44c3b53ecdc58ff97b2bea60d1c5646bf51a03
rbpolim <rbpolim@gmail.com>
2023-11-11 16:18:38 -0300

    feat: add data-table into financial page

---
+++ app/(dashboard)/financial/_components/columns.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+export type CategoryColumn = {
+  id: number
+  amount: number;
+  status: string;
+  month: string;
+  year: number;
+}
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "month",
+    header: "Mês",
+  },
+  {
+    accessorKey: "year",
+    header: "Ano",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "Status",
+    header: "Status",
+    cell: ({ row }) => row.original.status,
+  },
+];
--- app/(dashboard)/financial/page.tsx
+++ app/(dashboard)/financial/page.tsx
@@ -1,8 +1,65 @@
+import { DataTable } from "@/components/ui/data-table"
+import { Separator } from "@/components/ui/separator"
+
+import { columns } from "./_components/columns"
+
+export const data = [
+  {
+    id: 1,
+    amount: 259.99,
+    status: "ok",
+    month: "Janeiro",
+    year: 2023,
+  },
+  {
+    id: 1,
+    amount: 259.99,
+    status: "ok",
+    month: "Fevereiro",
+    year: 2023,
+  },
+  {
+    id: 1,
+    amount: 259.99,
+    status: "ok",
+    month: "Março",
+    year: 2023,
+  },
+  {
+    id: 1,
+    amount: 259.99,
+    status: "pending",
+    month: "Abril",
+    year: 2023,
+  },
+  {
+    id: 1,
+    amount: 259.99,
+    status: "pending",
+    month: "Maio",
+    year: 2023,
+  },
+  {
+    id: 1,
+    amount: 259.99,
+    status: "pending",
+    month: "Junho",
+    year: 2023,
+  },
+]
+
 const FinancialPage = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Financial Page</h1>
-    </div>
+    <div className="flex-1 p-8 pt-6 space-y-4">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Financial</h2>
+        <p className="text-sm text-muted-foreground">
+          Manage categories for your store
+        </p>
+      </div>
+      <Separator />
+      <DataTable searchKey="month" columns={columns} data={data} />
+    </div >
   )
 }
 
