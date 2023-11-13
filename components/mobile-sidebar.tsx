00a296d1bb01118f436082506c7fa19501d49f48
rbpolim <rbpolim@gmail.com>
2023-11-13 14:10:29 -0300

    style: adjusted dark mode sidebar component

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -13,7 +13,7 @@ export const MobileSidebar = () => {
       <SheetTrigger className="pr-4 transition md:hidden hover:opacity-75">
         <Menu />
       </SheetTrigger>
-      <SheetContent side="left" className="p-0 bg-white">
+      <SheetContent side="left" className="p-0">
         <Sidebar />
       </SheetContent>
     </Sheet>
